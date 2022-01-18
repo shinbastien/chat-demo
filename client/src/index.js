27eabef68f00fabe55d1d246f175d2f8d5a064ea
shinbastien <jugipalace@kaist.ac.kr>
2022-01-18 21:27:02 +0900

    edited project structure and successed showing videos

---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import Home from "./Home/Home";
+import ChatRoom from "./ChatRoom/ChatRoom";
+import Main from "./Main/Main";
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route exact path ="/" element = {<Home />} />
+      <Route exact path = ":roomID" element = {<Main/>} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ server/server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const {v4: uuidv4 } = require("uuid");
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const PORT = 4000;
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+const REMOVE_CHAT = "removeChat";
+
+const users = {};
+
+
+// Socket.io connection for Chatting
+io.on("connection", (socket) => {
+
+  // Join a conversation
+  const {GroupID, userName} = socket.handshake.query;
+  console.log("1", GroupID);
+  console.log("2", userName);
+
+  if (users[GroupID] && !users[GroupID].includes(userName)) {
+    users[GroupID].push({userName: userName, userID: socket.id})
+    console.log("user pushed in users in server")
+  }
+
+  else {
+    users[GroupID] = [{userName: userName, userID: socket.id}];
+  }
+
+  console.log("users in room", users[GroupID]);
+
+  // const usersInRoom = io.sockets.adapter.rooms.get( GroupID );
+  // console.log("3", usersInRoom);
+  
+  // send list of group members to each member 
+  io.emit("all users in group", users[GroupID]);
+
+  // when a peer is created from newbie, the created peer sends a signal by socket and socket sends the signal to existing peers
+  socket.on("sending signal", payload => {
+    io.to(payload.userIDToSignal).emit('user joined', {signal: payload.signal, callerID: payload.callerID, callerName: payload.callerName});
+    console.log("signal sended from newbie: ", payload.callerName);
+    console.log("to: ", payload.userNameToSignal);
+    console.log("to ID: ", payload.userIDToSignal);
+  })
+
+  socket.on("returning signal", payload => {
+    io.to(payload.callerID).emit('receiving returned signal', {signal: payload.signal, callerID: socket.id, callerName: userName})
+  })
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, ({body, senderId, senderName, ownedByCurrentUser}) => {
+    io.in(GroupID).emit(NEW_CHAT_MESSAGE_EVENT, {body, senderId, senderName, ownedByCurrentUser});
+  });
+
+  // Listen for remove message
+  socket.on(REMOVE_CHAT, (id) => {
+    io.in(GroupID).emit(REMOVE_CHAT, id);
+  });
+
+
+  // Leave the room if the user closes the socket
+  socket.on("disconnect", () => {
+    delete users[userName];
+    socket.leave(GroupID);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
