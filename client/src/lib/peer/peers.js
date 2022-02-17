fb91a776a4dcadbf293fec8a9639158c72617ace
shinbastien <jugipalace@kaist.ac.kr>
2022-02-17 14:52:49 +0900

    added refactored peer connection and files

---
+++ client/src/lib/peer/peers.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Peer from "simple-peer";
+
+
+function createPeer(roomName, userName, participants, socket, stream) {
+    const newPeers = {}
+
+    Object.keys(participants)
+    .filter((participantName) => participantName != userName)
+    .forEach((participantName) => {
+        console.log("gained user name: ", participantName);
+
+        const peer = new Peer({
+            initiator: true,
+            trickle: false,
+            stream,
+        })
+
+        peer.on("signal", (signal) => {
+            socket.emit("RTC_offer", signal, userName, participantName, roomName)
+            console.log("Offered RTC signal");
+            // socket.emit("sending signal", participants[participant].socket, participant, socket.id, userName, signal)
+        })
+
+        peer.on("connect", () => {
+            console.log("connected");
+        })
+    
+        // peer.on("stream", (stream) => {
+        //     peerStreams[participantName] = stream;
+
+        // })
+
+        peer.on("error", (err) => {
+            console.log(err);
+        })
+
+        console.log("create Peer of: ", participantName);
+
+        newPeers[participantName] = peer;
+    });
+
+    return newPeers
+}
+
+
+function addPeer(roomName, userName, participants, peers, socket, stream) {
+    let oldPeers = Object.keys(peers);
+    let newPeers = Object.keys(participants);
+
+    let newUser = newPeers.filter((x) => !oldPeers.includes(x) && x!= userName)[0];
+
+    const peer = new Peer({
+        initiator: false,
+        trickle: false,
+        stream,
+    });
+
+    // send returning signal from: existing to: newbie
+    peer.on("signal", (signal) => {
+        socket.emit("RTC_offer", signal, userName, newUser, roomName);
+    });
+    console.log(`${userName} adds Peer of : ${newUser}`);
+    
+    peer.on("connect", () => {
+        console.log("connected");
+    })
+
+    // peer.on("stream", (stream) => {
+    //     peerStreams[newUser] = stream;
+    // })
+
+    peer.on("error", (err) => {
+        console.log(err);
+    })
+
+    return {...peers, [newUser]: peer};
+}
+
+function disconnectPeer(peers, userName) {
+    delete peers[userName];
+
+    return peers;
+}
+
+export {addPeer, createPeer, disconnectPeer};
