9e325c3e7fd008f5320dc106f41fc4587f255e84
Eun Kang <choa030@gmail.com>
2022-02-23 23:43:29 +0900

    merge into main

--- client/src/Individual/Individual.jsx
+++ client/src/Individual/Individual.jsx
@@ -30,6 +30,26 @@ const BarWrapper = styled.div`
 	}
 `;
 
+// function TabPanel(props) {
+// 	const { children, value, index, ...other } = props;
+
+// 	return (
+// 		<div
+// 			role="tabpanel"
+// 			hidden={value !== index}
+// 			id={`simple-tabpanel-${index}`}
+// 			aria-labelledby={`simple-tab-${index}`}
+// 			{...other}
+// 		>
+// 			{value === index && (
+// 				<Box sx={{ p: 3 }}>
+// 					<Typography>{children}</Typography>
+// 				</Box>
+// 			)}
+// 		</div>
+// 	);
+// }
+
 export default function Individual({
 	individual,
 	stateChanger,
